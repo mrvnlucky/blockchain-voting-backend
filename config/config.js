e5daf39d0ec679b5791cd69e7de1509aa5aee6f9
Marvin Luckianto <marvnluck@gmail.com>
2023-11-18 22:49:17 +0700

    fix: update database config

--- config/config.js
+++ config/config.js
@@ -16,10 +16,7 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.MYSQL_HOST,
+    url: process.env.DATABASE_URL,
     dialect: "mysql",
     dialectOptions: {
       ssl: {
