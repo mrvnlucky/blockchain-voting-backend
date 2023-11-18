41f941b3be0735b15bd9941f7319c42260aea57f
Marvin Luckianto <marvnluck@gmail.com>
2023-11-18 23:59:51 +0700

    fix: db config url to var

--- config/config.js
+++ config/config.js
@@ -16,7 +16,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    url: process.env.DATABASE_URL,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
     dialect: "mysql",
     dialectOptions: {
       ssl: {
