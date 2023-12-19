9ee9907aec5235098725aa20295fdf430b7283c1
Marvin Luckianto <marvnluck@gmail.com>
2023-12-19 11:00:45 +0700

    fix: the same hashed results

--- utils/encryption.js
+++ utils/encryption.js
@@ -1,36 +1,33 @@
 const crypto = require("crypto");
 require("dotenv").config({ path: "../.env" });
 
-// Set encryption algorithm
+// Set encryption algorith
 const algorithm = "aes-256-cbc";
 const secret = `${process.env.ENCRYPTION_SECRET}`;
+const key = crypto
+  .createHash("sha256")
+  .update(String(secret))
+  .digest("base64")
+  .substr(0, 32);
 
 const encryptText = (text) => {
   const iv = crypto.randomBytes(16);
-
-  const cipher = crypto.createCipheriv(algorithm, secret, iv);
+  const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
 
   let encryptedData = cipher.update(text);
   encryptedData = Buffer.concat([encryptedData, cipher.final()]);
-
-  console.log("IV");
-  console.log(iv.toString("hex"));
-  console.log("encryptedData");
-  console.log(encryptedData.toString("hex"));
-
   return iv.toString("hex") + encryptedData.toString("hex");
 };
 
 const decryptText = (text) => {
   const iv = Buffer.from(text.substr(0, 32), "hex");
-
-  // Convert initialize vector from hex to hex
+  // Convert initialize vector from base64 to hex
   const encryptedData = Buffer.from(text.substr(32), "hex");
 
   // Decrypt the string using encryption algorithm and private key
-  const decipher = crypto.createDecipheriv(algorithm, secret, iv);
+  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
 
-  let decryptedData = decipher.update(encryptedData, "hex");
+  let decryptedData = decipher.update(encryptedData);
   decryptedData = Buffer.concat([decryptedData, decipher.final()]);
   return decryptedData.toString();
 };
